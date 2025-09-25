1ab2747cd08517e62720f7a0a24252b80e34307f
Eloy Santamaría <esantamaria1984@gmail.com>
2025-09-25 23:25:10 +0200

    Contacolor V.2 25/09/2025

--- app/views/home.php
+++ app/views/home.php
@@ -4,16 +4,52 @@
         <meta charset="utf-8">
         <title>Contacolor</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="css/home.css">
     </head>
     <body>
 
         <!--Cabecera de la página-->
-	    <div id="cabecera">
+	    <div id="head">
 		    <img src="/img/logo.png" id="logo">
 	    </div>
 
-            
+        <!--Cuerpo de la página-->
+        <div class="container">
+            <div class="boxes">
+                <div class="box" id="clients">
+                    <div>
+                        <h2>Clientes</h2>
+                        <p>Permite gestionar los diferentes clientes de la empresa</p>
+                        <a href="">Clientes</a>
+                    </div>
+                </div>
+                <div class="box" id="design">
+                    <div>
+                        <h2>Diseño</h2>
+                        <p>Permite introducir los costes de diseño gráfico</p>
+                        <a href="">Diseño</a>
+                    </div>
+                </div>
+                <div class="box" id="costs">
+                    <div>
+                        <h2>Costes</h2>
+                        <p>Permite introducir los costes de su máquina de imprimir</p>
+                        <a href="">Costes Impresión</a>
+                    </div>
+                </div>
+                <div class="box" id="papers">
+                    <div>
+                        <h2>Papeles</h2>
+                        <p>Permite gestionar los diferentes papeles</p>
+                        <a href="">Papeles</a>
+                    </div>
+                </div>
+            </div>
+            <div class="boxes">
+                <div class="box" id="manipulated"></div>
+                <div class="box" id="budgets"></div>
+            </div>
+        </div>            
 
     </body>
 </html>
